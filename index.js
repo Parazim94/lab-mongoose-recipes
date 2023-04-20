d40aa4391b6c6e31f9eb8d95bae6cb8551c6252c
Denis Röder <d-roeder@hotmail.de>
2023-04-20 16:01:13 +0200

    lab done

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
+const newRecipe = {
+  title: 'Icecream',
+  level: 'Easy Peasy',
+  ingredients: ['eggs', 'bacon'],
+  cuisine:'Spanish',
+  dishType: 'main_course',
+  image : 'https://images.media-allrecipes.com/userphotos/720x405/3489951.jpg',
+  duration: 20,
+  creator: 'Denis',
+};
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
@@ -17,7 +26,38 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    
+    return Recipe.create(newRecipe);
   })
+  .then((recipe) => {
+    console.log(`Add recipe: ${recipe.title}`);
+  })
+    .then(() => {
+      return Recipe.insertMany(data);
+    })
+    .then((recipes) => {
+      recipes.forEach((recipe) => {
+        console.log(`Add recipe: ${recipe.title}`);
+      });
+      return Recipe.findOneAndUpdate(
+        { title: 'Rigatoni alla Genovese' },
+        { duration: 100 },
+        { new: true }
+      );
+    })
+    .then(updatedRecipe => {
+      console.log(`Successfully updated recipe: ${updatedRecipe.title}`);
+      return Recipe.findOneAndDelete({title: 'Carrot Cake'});
+    })
+     .then(result=>{
+      console.log(result)
+      return mongoose.connection.close();
+    })
+    .then(() => console.log(`connection closed`))
+    
   .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  console.error('Error connecting to the database', error);
+ })
+  //.finally(() => {
+  //  mongoose.connection.close();
+  //});
